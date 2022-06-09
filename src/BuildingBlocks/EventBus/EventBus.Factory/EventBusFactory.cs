55b47db7100670ff42b9b3ebe3d9ae51881781a3
TheOnur <bulbuloff@gmail.com>
2022-06-09 22:48:14 +0300

    event bus rabbit mq implementation added.

---
+++ src/BuildingBlocks/EventBus/EventBus.Factory/EventBusFactory.cs
@@ -0,0 +1,21 @@
+﻿using EventBus.AzureServiceBus;
+using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.RabbitMQ;
+using System;
+
+namespace EventBus.Factory
+{
+    public static class EventBusFactory
+    {
+        public static IEventBus Create(EventBusConfig eventBusConfig, IServiceProvider serviceProvider)
+        {
+            return eventBusConfig.EventBusType switch
+            {
+                EventBusType.AzureServiceBus => new EventBusServiceBus(eventBusConfig, serviceProvider),
+                EventBusType.RabbitMQ => new EventBusRabbitMQ(eventBusConfig, serviceProvider),
+                _ => throw new NotImplementedException(),
+            };
+        }
+    }
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.RabbitMQ/DeliveryMode.cs
@@ -0,0 +1,8 @@
+﻿namespace EventBus.RabbitMQ
+{
+    public enum DeliveryMode
+    {
+        NonPersistent = 1,
+        Persistent = 2
+    }
+}
